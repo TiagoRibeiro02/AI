8b7db587faeed49283b5c3b4e59bd14c5581c1a6
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-09-26 22:24:20 +0100

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -11,3 +11,10 @@ for n in range(1, 17):
             while puzzle.count(randomvalue) != 0:
                 randomvalue = random.randint(0, 15)
         puzzle.append(randomvalue)
+        n=n+1
+
+def print_in_format(matrix):
+    for i in range(16):
+        if i %4 == 0 and i > 0:
+            print("")
+        print(str(matrix[i]) + " ", end = "")
