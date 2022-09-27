4e0f3e645f233162489d56dddab1ba25161913eb
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-09-27 22:15:00 +0100

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -33,32 +33,32 @@ def pos2d_to_1D(p,L,W):
     return (p[0]*W + p[1])
 
 def sucessors(state, L, W):
-    pos = state.find(0)
+    pos = state.index(0)
     pos_2d = pos1D_to_2D(pos, L, W)
     suc = []
     if(pos_2d[0] != 0): #norte
         nv = state.copy()
-        aux = state[pos2d_to_1D([pos_2d[0]-1, pos_2d[1]])]
+        aux = state[pos2d_to_1D((pos_2d[0]-1, pos_2d[1]), L, W)]
         nv[pos] = aux
-        nv[pos2d_to_1D([pos_2d[0]-1, pos_2d[1]])] = 0
+        nv[pos2d_to_1D((pos_2d[0]-1, pos_2d[1]), L, W)] = 0
         suc.append(nv)
-    if (pos_2d[0] != 2): #sul
+    if (pos_2d[0] != L-1): #sul
         nv = state.copy()
-        aux = state[pos2d_to_1D([pos_2d[0] + 1, pos_2d[1]])]
+        aux = state[pos2d_to_1D((pos_2d[0] + 1, pos_2d[1]), L, W)]
         nv[pos] = aux
-        nv[pos2d_to_1D([pos_2d[0] + 1, pos_2d[1]])] = 0
+        nv[pos2d_to_1D((pos_2d[0] + 1, pos_2d[1]), L, W)] = 0
         suc.append(nv)
-    if (pos_2d[1] != 1): #direita
+    if (pos_2d[1] != 0): #direita
         nv = state.copy()
-        aux = state[pos2d_to_1D([pos_2d[0], pos_2d[1] + 1])]
+        aux = state[pos2d_to_1D((pos_2d[0], pos_2d[1] + 1), L, W)]
         nv[pos] = aux
-        nv[pos2d_to_1D([pos_2d[0], pos_2d[1] + 1])] = 0
+        nv[pos2d_to_1D((pos_2d[0], pos_2d[1] + 1), L, W)] = 0
         suc.append(nv)
-    if (pos_2d[1] != 3): #esquerda
+    if (pos_2d[1] != W-1): #esquerda
         nv = state.copy()
-        aux = state[pos2d_to_1D([pos_2d[0], pos_2d[1] - 1])]
+        aux = state[pos2d_to_1D((pos_2d[0], pos_2d[1] - 1), L, W)]
         nv[pos] = aux
-        nv[pos2d_to_1D([pos_2d[0], pos_2d[1] - 1])] = 0
+        nv[pos2d_to_1D((pos_2d[0], pos_2d[1] - 1), L, W)] = 0
         suc.append(nv)
 
     return suc
@@ -67,5 +67,6 @@ def sucessors(state, L, W):
 
 state = create_random_state(L, W)
 show_state(state, L, W)
+print("\nsucessores", sucessors(state, L, W))
 
 
