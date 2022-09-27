2762dd3d452e72d739b5d8ee2eb9717a4d8f817f
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-09-27 10:57:10 +0100

    .

--- puzzle.py
+++ puzzle.py
@@ -1,30 +1,71 @@
 import random
 
-H=4
+L=4
 W=4
-casas = H * W
-state = []
 
-def create_random_valid_state():
-    for n in range(1, casas+1):
-        randomvalue = random.randint(0, casas-1)
-        if n == 1:
-            state.append(randomvalue)
-        if n > 1:
-            if state.count(randomvalue) != 0:
-                while state.count(randomvalue) != 0:
-                    randomvalue = random.randint(0, casas-1)
-            state.append(randomvalue)
+def create_random_state(L, W):
+    state = []
+    for n in range(L*W):
+        state.append(n)
+    random.shuffle(state)
+    return state
 
-
-def show_state(matrix):
-    for i in range(casas):
-        if i % 4 == 0 and i > 0:
+def show_state(state, L, W):
+    for i in range(L*W):
+        if i % L == 0 and i > 0:
             print("")
-        print(str(matrix[i]) + " ", end = "")
+        print(str(state[i]) + " ", end = "")
+
+def valid_state(state, L, W):
+    for i in range(len(state)):
+        if state.count(i) != 1:
+            valid = -1
+    if len(state) != L*W or valid == -1:
+        print("Invalid State")
+
+    else:
+        return state
+
+def pos1D_to_2D(p,L,W):
+    return ([p//W, p%W])
+
+def pos2d_to_1D(p,L,W):
+    return (p[0]*W + p[1])
+
+def sucessors(state, L, W):
+    pos = state.find(0)
+    pos_2d = pos1D_to_2D(pos, L, W)
+    suc = []
+    if(pos_2d[0] != 0): #norte
+        nv = state.copy()
+        aux = state[pos2d_to_1D([pos_2d[0]-1, pos_2d[1]])]
+        nv[pos] = aux
+        nv[pos2d_to_1D([pos_2d[0]-1, pos_2d[1]])] = 0
+        suc.append(nv)
+    if (pos_2d[0] != 2): #sul
+        nv = state.copy()
+        aux = state[pos2d_to_1D([pos_2d[0] + 1, pos_2d[1]])]
+        nv[pos] = aux
+        nv[pos2d_to_1D([pos_2d[0] + 1, pos_2d[1]])] = 0
+        suc.append(nv)
+    if (pos_2d[1] != 1): #direita
+        nv = state.copy()
+        aux = state[pos2d_to_1D([pos_2d[0], pos_2d[1] + 1])]
+        nv[pos] = aux
+        nv[pos2d_to_1D([pos_2d[0], pos_2d[1] + 1])] = 0
+        suc.append(nv)
+    if (pos_2d[1] != 3): #esquerda
+        nv = state.copy()
+        aux = state[pos2d_to_1D([pos_2d[0], pos_2d[1] - 1])]
+        nv[pos] = aux
+        nv[pos2d_to_1D([pos_2d[0], pos_2d[1] - 1])] = 0
+        suc.append(nv)
+
+    return suc
 
 
-create_random_valid_state()
-show_state(state)
+
+state = create_random_state(L, W)
+show_state(state, L, W)
 
 
