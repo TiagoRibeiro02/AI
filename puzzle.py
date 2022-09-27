abff75d6b551f62e706366f78a16dc6789545df9
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-09-27 09:28:54 +0100

    .

--- puzzle.py
+++ puzzle.py
@@ -1,20 +1,30 @@
 import random
 
-puzzle = []
+H=4
+W=4
+casas = H * W
+state = []
 
-for n in range(1, 17):
-    randomvalue = random.randint(0, 15)
-    if n == 1:
-        puzzle.append(randomvalue)
-    if n > 1:
-        if puzzle.count(randomvalue) != 0:
-            while puzzle.count(randomvalue) != 0:
-                randomvalue = random.randint(0, 15)
-        puzzle.append(randomvalue)
-        n=n+1
+def create_random_valid_state():
+    for n in range(1, casas+1):
+        randomvalue = random.randint(0, casas-1)
+        if n == 1:
+            state.append(randomvalue)
+        if n > 1:
+            if state.count(randomvalue) != 0:
+                while state.count(randomvalue) != 0:
+                    randomvalue = random.randint(0, casas-1)
+            state.append(randomvalue)
 
-def print_in_format(matrix):
-    for i in range(16):
-        if i %4 == 0 and i > 0:
+
+def show_state(matrix):
+    for i in range(casas):
+        if i % 4 == 0 and i > 0:
             print("")
         print(str(matrix[i]) + " ", end = "")
+
+
+create_random_valid_state()
+show_state(state)
+
+
