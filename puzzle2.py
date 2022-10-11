b026d5d597b150543ec43d9fd408e2ed893733e8
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-10-11 10:55:32 +0100

    Update puzzle2.py

--- puzzle2.py
+++ puzzle2.py
@@ -1,96 +1,71 @@
 import numpy as np
+from tabulate import tabulate
 
-
-def pos1_to_pos2(x, h, w):
-    row = x // w
-    col = x % w
-    return [row, col]
-
-
-def pos2_to_pos1(x2, h, w):
-    return x2[0] * w + x2[1]
+L=2
+W=2
 
 def create_random_state(h, w):
     t = np.arange(h * w)
     np.random.shuffle(t)
     return [list(t), []]
 
+def show_state(state, L, W):
+    for i in range(L*W):
+        if i % L == 0 and i > 0:
+            print("")
+        print(str(state[i]) + " ", end = "")
 
-def insert_state_tree(tr, nv, parent):
-    nd = find_node(tr, parent[0])
-    if nd is None:
-        return None
-    nd[1].append(nv)
-    return tr
+def valid_state(state, L, W):
+    valid =0
+    for i in range(len(state)):
+        if state.count(i) != 1:
+            valid = -1
+    if len(state) != L*W or valid == -1:
+        print("Invalid State")
 
+    else:
+        return state
 
-def get_successors(t, h, w):
-    ret = []
-    pos_1d = t.index(0)
-    pos_2d = pos1_to_pos2(pos_1d, h, w)
-    if pos_2d[0] > 0:
-        nv = t.copy()
-        aux = nv[pos2_to_pos1([pos_2d[0] - 1, pos_2d[1]], h, w)]
-        nv[pos2_to_pos1([pos_2d[0] - 1, pos_2d[1]], h, w)] = 0
-        nv[pos_1d] = aux
-        ret.append(nv)
+def pos1D_to_pos2D(x,l,w):
+    row = x // w
+    col = x % w
+    return [row, col]
 
-    if pos_2d[1] > 0:
-        nv = t.copy()
-        aux = nv[pos2_to_pos1([pos_2d[0], pos_2d[1] - 1], h, w)]
-        nv[pos2_to_pos1([pos_2d[0], pos_2d[1] - 1], h, w)] = 0
-        nv[pos_1d] = aux
-        ret.append(nv)
-
-    if pos_2d[0] < h - 1:
-        nv = t.copy()
-        aux = nv[pos2_to_pos1([pos_2d[0] + 1, pos_2d[1]], h, w)]
-        nv[pos2_to_pos1([pos_2d[0] + 1, pos_2d[1]], h, w)] = 0
-        nv[pos_1d] = aux
-        ret.append(nv)
-
-    if pos_2d[1] < w - 1:
-        nv = t.copy()
-        aux = nv[pos2_to_pos1([pos_2d[0], pos_2d[1] + 1], h, w)]
-        nv[pos2_to_pos1([pos_2d[0], pos_2d[1] + 1], h, w)] = 0
-        nv[pos_1d] = aux
-        ret.append(nv)
-
-    return ret
-
-def expand_tree(tr, n, h, w):
-    if n == 0:
-        return tr
-    suc = get_successors(tr[0], h, w)
-    for s in suc:
-        tr = insert_state_tree(tr, expand_tree([s, []], n-1, h, w), tr)
-    return tr
+def pos2D_to_pos1D(x2,l,w):
+    return x2[0] * w + x2[1]
 
 
+def successors(state, h, w):
+    pos_0 = state.index(0)
+    pos0_2D = pos1D_to_pos2D(pos_0, h, w)
+    suc = []
+    if pos0_2D[0] != 0:  # mover para cima
+        mv = state.copy()
+        aux = state[pos2D_to_pos1D([pos0_2D[0] - 1, pos0_2D[1]], h, w)]
+        mv[pos_0] = aux
+        mv[pos2D_to_pos1D((pos0_2D[0] - 1, pos0_2D[1]), h, w)] = 0
+        suc.append(mv)
+    if pos0_2D[1] != w - 1:  # mover para a direita
+        mv = state.copy()
+        aux = state[pos2D_to_pos1D([pos0_2D[0], pos0_2D[1] + 1], h, w)]
+        mv[pos_0] = aux
+        mv[pos2D_to_pos1D((pos0_2D[0], pos0_2D[1] + 1), h, w)] = 0
+        suc.append(mv)
+    if pos0_2D[0] != h - 1:  # mover para baixo
 
-def count_nodes(tr):
-    ret = 0
-    if len(tr) > 0:
-        for t in tr[1]:
-            ret += count_nodes(t)
-        return(1 + ret)
-    return ret
+        mv = state.copy()
+        aux = state[pos2D_to_pos1D([pos0_2D[0] + 1, pos0_2D[1]], h, w)]
+        mv[pos_0] = aux
+        mv[pos2D_to_pos1D((pos0_2D[0] + 1, pos0_2D[1]), h, w)] = 0
+        suc.append(mv)
+    if pos0_2D[1] != 0:  # mover para esquerda
+        mv = state.copy()
+        aux = state[pos2D_to_pos1D([pos0_2D[0], pos0_2D[1] - 1], h, w)]
+        mv[pos_0] = aux
+        mv[pos2D_to_pos1D((pos0_2D[0], pos0_2D[1] - 1), h, w)] = 0
+        suc.append(mv)
 
-
-
-def show_tree(tr, cur_d, h, w):
-    if len(tr) == 0:
-        return
-    print('[%d]-------------------------------------' % cur_d)
-    for ih in range(h):
-        for iw in range(w):
-            print('%d\t' % tr[0][ih * w + iw], end='')
-        print('')
-    print('-------------------------------------')
-    for t in tr[1]:
-        show_tree(t, cur_d +1, h, w)
-
-
+    return suc
 
 def find_node(tr, id):
     if len(tr) == 0:
@@ -102,16 +77,68 @@ def find_node(tr, id):
         if aux is not None:
             return aux
     return None
+def insert_tree(Tree, New, Father):
+    nd = find_node(Tree, Father[0])
+    if nd is None:
+        return None
+    nd[1].append(New)
+    return Tree
+
+def show_tree(tr, cur_d, h, w):
+    if len(tr) == 0:
+        return
+    print('[%d]-------------------------------------' % cur_d)
+    for ih in range(h):
+        for iw in range(w):
+            print('%d\t' % tr[0][ih * w + iw], end='')
+        print('')
+    print('-------------------------------------')
+    for t in tr[1]:
+        show_tree(t, cur_d + 1, h, w)
+
+def count_tree(Tree):
+    ret = 0
+    if len(Tree) > 0:
+        for t in Tree[1]:
+            ret += count_tree(t)
+        return (1 + ret)
+    return ret
+def expand_tree(tr, n, h, w):
+    if n == 0:
+        return tr
+    suc = successors(tr[0], h, w)
+    for s in suc:
+        tr = insert_tree(tr, expand_tree([s, []], n-1, h, w), tr)
+    return tr
+
+def is_final_state(tab):
+    aux = list(np.arange(len(tab)))
+    return tab[:-1] == aux[1:]
+
+def BFS (state, h, w):
+    done = []
+    todo = [state]
+
+    while len(todo) > 0:
+        x = list.pop(todo)
+        if x == is_final_state(x):
+            return x
+        suc = successors(x, h, w)
+        for s in suc:
+            if s in done:
+                continue
+            todo.append(s)
+        done.append(x)
+    return None
 
 
+st = create_random_state(L, W)
 
-width = 4
-height = 4
+##st = expand_tree(st, 2, L, W)
 
-st = create_random_state(height, width)
+##show_tree(st, 0, L, W)
 
-st = expand_tree(st, 2, height, width)
+show_state(BFS(st, L, W), L, W)
 
-show_tree(st, 0, height, width)
 
-print('Total nodes: %d' % count_nodes(st))
+print('Total nodes: %d' % count_tree(st))
